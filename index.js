bff8f842ccb8146cb83b798a40b82766688d54e3
Timothy Liebermann <tliebermann.33@gmail.com>
2023-08-15 21:49:56 +0100

    Exercises 3.13-3.14

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
+const person = require("./models/person");
 
 const app = express();
 app.use(cors());
@@ -17,69 +20,31 @@ app.use(
     )
 );
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-    },
-];
-
 app.get("/api/persons", (request, response) => {
-    response.json(persons);
+    person.find({}).then((result) => response.json(result));
 });
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find((person) => person.id === id);
-
-    if (person) {
+    person.findById(request.params.id).then((person) => {
         response.json(person);
-    } else {
-        response.status(404).end();
-    }
+    });
 });
 
 app.post("/api/persons", (request, response) => {
-    const person = request.body;
+    const body = request.body;
 
-    if (!person.name) {
-        return response.status(400).json({
-            error: "name missing",
-        });
+    if (body.name === undefined) {
+        return response.status(400).json({ error: "content missing" });
     }
 
-    if (!person.number) {
-        return response.status(400).json({
-            error: "number missing",
-        });
-    }
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+    });
 
-    if (persons.find((p) => p.name === person.name) !== undefined) {
-        return response.status(400).json({
-            error: "name must be unique",
-        });
-    }
-
-    person.id = Math.round(Math.random() * 100000);
-
-    persons = persons.concat(person);
-    response.json(person);
+    person.save().then((savedPerson) => {
+        response.json(savedPerson);
+    });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
