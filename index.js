4acbdaed5df91d5970276045b5484e927f520b98
Timothy Liebermann <tliebermann.33@gmail.com>
2023-08-16 16:05:30 +0100

    Exercises 3.15-3.18

--- index.js
+++ index.js
@@ -20,17 +20,27 @@ app.use(
     )
 );
 
-app.get("/api/persons", (request, response) => {
-    person.find({}).then((result) => response.json(result));
+app.get("/api/persons", (request, response, next) => {
+    person
+        .find({})
+        .then((result) => response.json(result))
+        .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
-    person.findById(request.params.id).then((person) => {
-        response.json(person);
-    });
+app.get("/api/persons/:id", (request, response, next) => {
+    person
+        .findById(request.params.id)
+        .then((person) => {
+            if (person) {
+                response.json(person);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
     const body = request.body;
 
     if (body.name === undefined) {
@@ -42,27 +52,60 @@ app.post("/api/persons", (request, response) => {
         number: body.number,
     });
 
-    person.save().then((savedPerson) => {
-        response.json(savedPerson);
-    });
+    person
+        .save()
+        .then((savedPerson) => {
+            response.json(savedPerson);
+        })
+        .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    person = persons.filter((person) => person.id !== id);
-
-    response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then((result) => {
+            response.status(204).end();
+        })
+        .catch((error) => next(error));
 });
 
-app.get("/info", (request, response) => {
-    response.send(
-        `<p>Phonebook has info for ${
-            persons.length
-        } people</p><p>${new Date().toLocaleString()}</p>`
-    );
+app.put("/api/persons/:id", (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    };
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then((updatedPerson) => {
+            response.json(updatedPerson);
+        })
+        .catch((error) => next(error));
+});
+
+app.get("/info", (request, response, next) => {
+    person
+        .find({})
+        .then((savedPersons) =>
+            response.send(
+                `<p>Phonebook has info for ${
+                    savedPersons.length
+                } people</p><p>${new Date().toLocaleString()}</p>`
+            )
+        )
+        .catch((error) => next(error));
 });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+    return response.status(400).send({ error: "an unexpected error occured" });
+
+    next(error);
+};
+
+app.use(errorHandler);
