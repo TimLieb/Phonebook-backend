7f048a6e8693a2d80be51f8868d8fc12d9f69a33
Tim Liebermann <tim.liebermann@ramboll.co.uk>
2023-08-17 10:58:37 +0100

    uibuild

--- index.js
+++ index.js
@@ -103,6 +103,11 @@ app.listen(PORT, () => {
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message);
+
+    if (error.name === "ValidationError") {
+        return response.status(400).json({ error: error.message });
+    }
+
     return response.status(400).send({ error: "an unexpected error occured" });
 
     next(error);
